15f7c030a71dac2ff34fe1ab44da0709215b1d78
kerwinagpalasin <143233626+kerwinagpalasin@users.noreply.github.com>
2023-10-21 17:43:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+var selectedRow = null
+
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
+}
+
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["Age"] = document.getElementById("Age").value;
+    formData["Gender"] = document.getElementById("Gender").value;
+    formData["School Year"] = document.getElementById("SchoolYear").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.Age;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.Gender;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.SchoolYear;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a onClick="onEdit(this)">Edit</a>
+                       <a onClick="onDelete(this)">Delete</a>`;
+}
+
+function resetForm() {
+    document.getElementById("fullName").value = "";
+    document.getElementById("Age").value = "";
+    document.getElementById("Gender").value = "";
+    document.getElementById("School Year").value = "";
+    selectedRow = null;
+}
+
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("Age").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("Gender").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("SchoolYear").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.Age;
+    selectedRow.cells[2].innerHTML = formData.Gender;
+    selectedRow.cells[3].innerHTML = formData.SchoolYear;
+}
+
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("employeeList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+
+function validate() {
+    isValid = true;
+    if (document.getElementById("fullName").value == "") {
+        isValid = false;
+        document.getElementById("fullNameValidationError").classList.remove("post");
+    } else {
+        isValid = true;
+        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
+            document.getElementById("fullNameValidationError").classList.add("hide");
+    }
+    return isValid;
+}
